0f3d86a8025db704ca985e8b4bb90f24a0a8373d
rainbow-bamboo <61608357+rainbow-bamboo@users.noreply.github.com>
2021-06-18 23:47:55 +0000

    validates passcode

--- src/components/rules/closet.cljs
+++ src/components/rules/closet.cljs
@@ -1,1 +1,13 @@
-(ns components.rules.closet)
+(ns components.rules.closet
+  (:require [odoyle.rules :as o]))
+
+(def closet-rules
+  (o/ruleset
+   {::check-passcode
+    [:what
+     [::global ::correct-passcode correct]
+     [::global ::inserted-passcode inserted]
+     :then
+     (if (= correct (reduce str inserted))
+       (o/insert! ::global ::valid-passcode? true)
+       nil)]}))
--- src/components/rules/core.cljs
+++ src/components/rules/core.cljs
@@ -6,13 +6,14 @@
             [components.rules.closet :as c]))
 
 (def initial-session
-  (-> (reduce o/add-rule (o/->session) (concat  t/todo-rules e/event-rules r/render-rules))
+  (-> (reduce o/add-rule (o/->session) (concat  t/todo-rules e/event-rules r/render-rules c/closet-rules))
       (o/insert ::t/global {::t/new-todo ""
                             ::t/active-id nil})
       (o/insert ::t/derived {::t/todos []})
       (o/insert ::e/global {::e/next-id 1})
       (o/insert ::c/global {::c/inserted-passcode []
-                            ::c/correct-passcode ["P" "R" "I" "D" "E"]})
+                            ::c/correct-passcode "PRIDE"
+                            ::c/valid-passcode? false})
       o/fire-rules))
 
 (def *todo-session (atom initial-session))
